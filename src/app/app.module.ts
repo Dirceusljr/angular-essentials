7ec2e650e49d00703cca34fef30f2bbdafa6b24c
dirceusljr <dirceusljr@gmail.com>
2024-10-02 19:07:43 -0300

    refactor: :recycle: refactor: combining Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TasksModule } from './tasks/tasks.module';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
-import { CardComponent } from './shared/card/card.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    CardComponent,
-    TasksComponent,
-    NewTaskComponent,
-    TaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
